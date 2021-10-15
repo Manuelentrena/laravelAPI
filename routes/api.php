45feee196aba9570dac3e68bc016a3d95b619145
Manuel Entrena <manuel.entrena@gmail.com>
2021-10-15 08:35:43 +0200

    Added rutes Api V1 example

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::apiResource('v1/posts', Api\V1\PostController::class);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/* Route::prefix('v2')->group(function () {
+    Route::apiResource('posts', Api\V1\PostController::class);
+}); */
